175315bee589a5d677faa906692089da7275dc33
sahand maleki <xdellbrindar@gmail.com>
2024-03-13 12:40:18 +0330

    instagram profile downloader

---
+++ main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+import instaloader
+import urllib
+from urllib.request import urlopen
+from PIL import Image,ImageTk
+import io
+
+def get_image():
+    L=instaloader.Instaloader()
+    L.login('sahand_m_teach','qwert1234')
+    profile=instaloader.Profile.from_username(L.context, f"{username.get()}")
+    a=urlopen(profile.get_profile_pic_url())
+    data=a.read()
+    a.close()
+    image=Image.open(io.BytesIO(data))
+    pic=ImageTk.PhotoImage(image)
+    Label.config(image=pic)
+    label.image=pic
+    label.pack()
+
+window=Tk()
+window.title('instagram profile downloader')
+window.geometry('600x600')
+Label(window,text='enter your instagram username').pack()
+username=Entry(window,width=40)
+username.pack()
+button=Button(window,text='start download')
+button.pack()
+button.config(command=get_image)
+label=Label(window)
+
+window.mainloop()
+
